276e6fe775e668569465310680d36ff8216807ee
tanvirGHD <155833719+tanvirGHD@users.noreply.github.com>
2024-12-25 18:19:20 +0600

    JWT implements

--- index.js
+++ index.js
@@ -1,15 +1,42 @@
 
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173'],
+  credentials: true
+}));
 app.use(express.json());
+app.use(cookieParser());
+
+
+const verifyToken = (req, res, next) =>{
+  const token = req.cookies?.token;
+  
+  if(!token){
+    return res.status(401).send({message: 'unauthorized access'});
+  }
+
+  // verify the token 
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if(err){
+      return res.status(401).send({message: 'unauthorized access'});
+    }
+    
+    next();
+  })
+}
+
+
 
 // MongoDB URI
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.y15rh.mongodb.net/?retryWrites=true&w=majority`;
@@ -31,12 +58,38 @@ async function run() {
     const carsCollection = client.db('carRent').collection('cars');
     const bookingCarCollection = client.db('carRent').collection('cars-booking');
 
+
+    //Auth related API
+    app.post('/jwt', (req, res) =>{
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '5h'});
+
+      res.cookie('token', token, {
+        httpOnly: true,
+        secure: false
+      })
+      .send({success: true})
+    })
+
+
+    app.post('/logout', (req, res)=>{
+      res.clearCookie('token', {
+        httpOnly: true,
+        secure: false
+      })
+      .send({success: true})
+    })
+
+
+
     // Get all cars
-    app.get('/cars', async (req, res) => {
+    app.get('/cars', verifyToken, async (req, res) => {
       const cursor = carsCollection.find();
+      // console.log(req.cookies?.token);
       const result = await cursor.toArray();
       res.send(result);
     });
+
 
     //car booking
     app.get('/cars-booking', async (req, res) => {
